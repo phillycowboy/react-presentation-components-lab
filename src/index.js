41207189aebc12790f92c29935932534ed8ae6d8
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-06-22 22:01:54 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+handleOtherClick = () => {
+  alert("AHHHHHH!")
+}
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={this.handleOtherClick} />
   </div>,
   document.getElementById('root')
-);
+  );
